a7aaff9d3af6d78d6d0840e4744dd04d6f7faaad
John-Rashta <john_rashta@outlook.com>
2024-07-02 22:34:51 +0100

    Add basic card generation
    Added basic card generation with the main function of the page,
    makeCard which makes a card and adds it to the page along with all
    the numbers necessary, everything seems to be working, same for the
    toggle read and delete buttons, only some styling missing now

--- library/library.js
+++ library/library.js
@@ -4,33 +4,88 @@ function Book(title, author, pages, read) {
     this.title = title;
 	this.author = author;
 	this.pages = pages;
-	this.read = boolean(read);
+	this.read = Boolean(read);
 }
+
 
 function addBookToLibrary(title, author, pages, read) {
 
     const userBook = new Book(title, author, pages, read);
     myLibrary.push(userBook);
+    makeCard(userBook);
 }
 
+///Clear grid and loop to display every book
 function displayBooks() {
+
+    const bookGrid = document.querySelector(".main");
+    while (bookGrid.firstChild) {
+        bookGrid.removeChild(bookGrid.firstChild);
+    }
+    myLibrary.forEach((book) => makeCard(book));
 
     
 }
 
 function makeCard(book) {
+    const main = document.querySelector(".main");
 
+    const mainDiv = document.createElement("div");
+
+    const titleDiv = document.createElement("div");
+    titleDiv.textContent = book.title;
+
+    const authorDiv = document.createElement("div");
+    authorDiv.textContent = book.author;
+
+    const pagesDiv = document.createElement("div");
+    pagesDiv.textContent = `${book.pages} pages`;
+
+    const readDiv = document.createElement("div");
+    readDiv.textContent = "Read";
+    readDiv.classList.toggle("reading");
+    
+    const buttonsDiv = document.createElement("div");
+    buttonsDiv.classList.toggle("buttons-container");
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
 
     const readButton = document.createElement("button");
     readButton.textContent = "Read";
+    
     if (book.read) {
-        readButton.classList.toggle("read");
+        readDiv.classList.toggle("read");
     }
-    readButton.addEventListener("click", (event) => {
+    
+    readButton.addEventListener("click", () => {
         book.read = !book.read;
-        event.target.classList.toggle("read");
+        readDiv.classList.toggle("read");
        
     })
+
+    deleteButton.addEventListener("click", () => {
+
+        mainDiv.parentNode.removeChild(mainDiv);
+        let bookIndex = myLibrary.indexOf(book);
+        myLibrary.splice(bookIndex, 1);
+
+
+    })
+
+    buttonsDiv.appendChild(deleteButton);
+    buttonsDiv.appendChild(readButton);
+
+
+    mainDiv.appendChild(titleDiv);
+    mainDiv.appendChild(authorDiv);
+    mainDiv.appendChild(pagesDiv);
+    mainDiv.appendChild(readDiv);
+    mainDiv.appendChild(buttonsDiv);
+
+    main.appendChild(mainDiv);
+    
+
 
 }
 
@@ -45,11 +100,17 @@ addButton.addEventListener("click", () => {
 
 });
 
+/// Acess formdata when submit is pressed and send it to AddBook function
 submitButton.addEventListener("click", (event) => {
 
     event.preventDefault();
     const bookform = document.querySelector("#bookform");
     const formData = new FormData(bookform);
+    let title = formData.get("title");
+    let author = formData.get("author");
+    let pages = Number(formData.get("pages"));
+    let read = (formData.get("status") === "true");
+    addBookToLibrary(title, author, pages, read);
     dialog.close();
 
 })
